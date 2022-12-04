7ad28640b3eae3d9c997a8c2e235d770ec61749f
Josekariz <joseph.macharia@student.moringaschool.com>
2022-12-04 21:47:07 +0300

    render base router component

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,15 @@
-import React, { useEffect, useState } from "react";
-import Login from "../login/Login";
-import SignUp from "../signup/SignUp.js";
+import BaseRouter from "../routes";
+import { BrowserRouter as Router } from "react-router-dom";
+import React from "react";
 
-function App() {  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    // auto-login
-    fetch("/me").then((r) => {
-      if (r.ok) {
-        r.json().then((user) => setUser(user));
-      }
-    });
-  }, []);
-
-//  if (!user) return <Login onLogin={setUser} />;
-
-
- return(<>
- <SignUp/>
-  </>
-  )
+function App() {
+  return (
+    <>
+      <Router>
+        <BaseRouter />
+      </Router>
+    </>
+  );
 }
 
 export default App;
