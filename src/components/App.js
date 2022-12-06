5308305a07782f920570aaa3877c0161c9a54ef9
Josekariz <joseph.macharia@student.moringaschool.com>
2022-12-06 11:03:52 +0300

    move auto-login to app component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,22 @@
 import BaseRouter from "../routes";
 import { BrowserRouter as Router } from "react-router-dom";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    // auto-login
+    fetch("/me").then((r) => {
+      if (r.ok) {
+        r.json().then((user) => setUser(user));
+      }
+    });
+  }, []);
   return (
     <>
       <Router>
-        <BaseRouter />
+        <BaseRouter user={user} setUser={setUser} />
       </Router>
     </>
   );
