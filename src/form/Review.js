fce7bffb0181a137661252889da210b5823bae1a
Josekariz <joseph.macharia@student.moringaschool.com>
2022-12-08 10:45:36 +0300

    edit typo in review form

--- src/form/Review.js
+++ src/form/Review.js
@@ -59,7 +59,7 @@ function Review() {
                 value={name}
                 onChange={(e) => setName(e.target.value)}
               />
-              <label for="image_url">Model</label>
+              <label for="image_url">Image</label>
               <input
                 onClick={handleClear}
                 placeholder="Image url"
